ee4a5467fa080e078cb3e342d99578e00f3e38cf
Nicole Dávila Hernández <a01784217@tec.mx>
2024-10-21 17:14:23 -0600

    initial commit

---
+++ openai/basicopenai.py
@@ -0,0 +1,21 @@
+from openai import OpenAI
+
+secret_key=""
+
+prompt = "give me a summary of the first shrek movie (2001)"
+
+client = OpenAI(
+    api_key = secret_key,
+    )
+
+completion = client.chat.completions.create(
+    model="gpt-3.5-turbo",  
+    messages=[
+        {"role": "system", "content": "You are a helpful assistant."},
+        {"role": "user", "content": prompt}
+    ],
+    max_tokens=100,
+    temperature=1
+)
+
+print(completion.choices[0].message.content)
